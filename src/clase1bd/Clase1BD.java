1e15b4662b0f991a24595bf05dd540e35451c4cd
Labing I5 <Labing I5@LabingI5-0-101>
2017-07-24 09:57:32 -0500

    Primera version - Hola Mundo

---
+++ src/clase1bd/Clase1BD.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1bd;
+
+/**
+ *
+ * @author Labing I5
+ */
+public class Clase1BD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo");
+             
+    }
+    
+}
